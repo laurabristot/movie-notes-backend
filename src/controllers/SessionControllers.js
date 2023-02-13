d520d4f6f7971a120fc9892cb1c4d379e1883af6
laura bristot <laurabristotbarbi@hotmail.com>
2023-02-13 16:40:02 -0300

    token do usuário

--- src/controllers/SessionControllers.js
+++ src/controllers/SessionControllers.js
@@ -1,23 +1,32 @@
-const { compare } = require('bcryptjs');
-const knex = require('../database/knex');
+const { compare } = require('bcryptjs')
+const knex = require('../database/knex')
+const authConfig = require('../configs/auth')
+const { sign } = require('jsonwebtoken')
 
 class SessionControllers {
-  async create(req, res){
+  async create(req, res) {
     const { email, password } = req.body
-    
-    const user = await knex('users').where({email}).first()
 
-    if(!user){
+    const user = await knex('users').where({ email }).first()
+
+    if (!user) {
       return res.status(401).json('Email/Senha não confere')
     }
 
     const passwordMatched = await compare(password, user.password)
 
-    if(!passwordMatched){
+    if (!passwordMatched) {
       return res.status(401).json('Email/Senha não confere')
     }
 
-    return res.json(user)
+
+    const { secret, expiresIn } = authConfig.jwt
+    const token = sign({}, secret, {
+      subject: String(user.id),
+      expiresIn
+    })
+
+    return res.json({user, token})
   }
 }
 
