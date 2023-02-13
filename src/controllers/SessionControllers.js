f16a9af293f8efd95ec47ba158fa18cddc259e6d
laura bristot <laurabristotbarbi@hotmail.com>
2023-02-13 14:40:41 -0300

    validando email e senha

--- src/controllers/SessionControllers.js
+++ src/controllers/SessionControllers.js
@@ -1,9 +1,23 @@
+const { compare } = require('bcryptjs');
+const knex = require('../database/knex');
+
 class SessionControllers {
   async create(req, res){
     const { email, password } = req.body
- 
-    return res.json({email, password})
- 
+    
+    const user = await knex('users').where({email}).first()
+
+    if(!user){
+      return res.status(401).json('Email/Senha não confere')
+    }
+
+    const passwordMatched = await compare(password, user.password)
+
+    if(!passwordMatched){
+      return res.status(401).json('Email/Senha não confere')
+    }
+
+    return res.json(user)
   }
 }
 
